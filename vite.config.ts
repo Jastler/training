bbe2f18358e6df569046562b9092cbff233ea621
Taras <chirkovtaras@gmail.com>
2025-01-30 14:19:40 +0200

    feat: change vite config and package json

--- vite.config.ts
+++ vite.config.ts
@@ -4,57 +4,65 @@ import visualizer from "rollup-plugin-visualizer";
 import path from "path";
 import { configDefaults } from "vitest/config";
 
-export default defineConfig({
-  plugins: [
-    react(),
-    visualizer({
-      open: true,
-      filename: "bundle-report.html",
-      gzipSize: true,
-      brotliSize: true,
-    }),
-  ],
-  resolve: {
-    alias: {
-      "@store": path.resolve(__dirname, "src/store"),
-      "@components": path.resolve(__dirname, "src/components"),
-      "@features": path.resolve(__dirname, "src/features"),
-      "@hooks": path.resolve(__dirname, "src/hooks"),
-      "@services": path.resolve(__dirname, "src/services"),
-      "@styles": path.resolve(__dirname, "src/styles"),
-      "@utils": path.resolve(__dirname, "src/utils"),
-      "@pages": path.resolve(__dirname, "src/pages"),
+export default defineConfig(({ mode }) => {
+  const plugins: any[] = [react()];
+
+  if (process.env.ANALYZE === "true") {
+    plugins.push(
+      visualizer({
+        open: true,
+        filename: "bundle-report.html",
+        gzipSize: true,
+        brotliSize: true,
+      }) as any
+    );
+  }
+
+  return {
+    plugins,
+    resolve: {
+      alias: {
+        "@store": path.resolve(__dirname, "src/store"),
+        "@components": path.resolve(__dirname, "src/components"),
+        "@features": path.resolve(__dirname, "src/features"),
+        "@hooks": path.resolve(__dirname, "src/hooks"),
+        "@services": path.resolve(__dirname, "src/services"),
+        "@styles": path.resolve(__dirname, "src/styles"),
+        "@utils": path.resolve(__dirname, "src/utils"),
+        "@pages": path.resolve(__dirname, "src/pages"),
+      },
     },
-  },
-  build: {
-    outDir: "dist",
-    emptyOutDir: true,
-    rollupOptions: {
-      treeshake: true,
-      output: {
-        manualChunks(id) {
-          if (id.includes("node_modules")) {
-            if (id.includes("react") || id.includes("react-dom"))
-              return "react";
-            if (id.includes("firebase")) return "firebase";
-            if (id.includes("@mui")) return "mui";
-            return "vendor";
-          }
+    build: {
+      sourcemap: false,
+      outDir: "dist",
+      emptyOutDir: true,
+      rollupOptions: {
+        treeshake: true,
+        output: {
+          manualChunks(id) {
+            if (id.includes("node_modules")) {
+              if (id.includes("react") || id.includes("react-dom"))
+                return "react";
+              if (id.includes("firebase")) return "firebase";
+              if (id.includes("@mui")) return "mui";
+              return "vendor";
+            }
+          },
         },
       },
     },
-  },
-  publicDir: "public",
-  server: {
-    port: 3000,
-    open: true,
-    host: true,
-    allowedHosts: ["7d75-79-110-129-250.ngrok-free.app"],
-  },
-  test: {
-    globals: true,
-    environment: "jsdom",
-    setupFiles: "./src/test/setup.ts",
-    exclude: [...configDefaults.exclude, "node_modules/**"],
-  },
+    publicDir: "public",
+    server: {
+      port: 3000,
+      open: true,
+      host: true,
+      allowedHosts: ["7d75-79-110-129-250.ngrok-free.app"],
+    },
+    test: {
+      globals: true,
+      environment: "jsdom",
+      setupFiles: "./src/test/setup.ts",
+      exclude: [...configDefaults.exclude, "node_modules/**"],
+    },
+  };
 });
